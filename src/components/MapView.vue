43029e915d9523851b273a2aba501be4e2b35cc5
Raiden Fumo <herrscher.of.sleeping@protonmail.com>
2025-08-23 07:06:48 +0200

    Add overlay dot on top of opened location on the map

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -6,6 +6,7 @@ import { store } from "@/store";
 import { makeUrl } from "@/url";
 
 const coords: Ref<null|Point> = ref(null);
+const isOverlayVisible = ref(true);
 
 function getDisplayStyle(coords: null|Point) {
   return coords === null ? "none" : "";
@@ -13,8 +14,8 @@ function getDisplayStyle(coords: null|Point) {
 
 subscribe("set-display-point", function(point: Point|null) {
   coords.value = point;
+  isOverlayVisible.value = true;
 });
-
 </script>
 
 <template>
@@ -24,6 +25,45 @@ subscribe("set-display-point", function(point: Point|null) {
         <option v-for="zoom in 11" :key="zoom" :value="zoom">{{ zoom }}</option>
       </select>
     </div>
+
     <iframe :src="makeUrl(coords)" frameborder="0" style="width: 100%; height: 100%; display: block; position: absolute; top: 0; left: 0;"></iframe>
+    <div class="overlay" :class="{'overlay-hidden': !isOverlayVisible }" @click="isOverlayVisible=false">
+      <div class="map-target"></div>
+      <div class="map-target map-target-inside-dot"></div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.overlay {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.1);
+  z-index: 1000;
+}
+
+.overlay-hidden {
+  display: none;
+}
+
+.map-target {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: purple;
+  padding: 5px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.map-target-inside-dot {
+  padding: 2px;
+  border-radius: 2px;
+  background-color: white
+}
+</style>
